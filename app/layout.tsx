6b446297d438ecbc936436d445de4cf0338e8e4b
hathbanger <hathbanger.ah@gmail.com>
2024-12-03 12:38:29 -0700

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,7 +42,7 @@ const interDisplay = localFont({
 // Dynamic Metadata Export
 export const metadata = {
   title: "Formation",
-  description: "The fog is coming.",
+  description: "A Fog is Forming.",
 };
 
 export default function RootLayout({
