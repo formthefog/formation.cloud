aa2da00cae79b825126d6a39b8dc0b678b78cefa
hathbanger <hathbanger.ah@gmail.com>
2025-04-06 16:01:18 +0200

    fix for build

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,10 +10,8 @@ export function Providers({ children }: { children: React.ReactNode }) {
         environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ENVIRONMENT_ID!,
         walletConnectors: [EthereumWalletConnectors],
         displaySiweStatement: true,
-        storageKey: 'formation.cloud.auth',
         appName: 'Formation',
         appLogoUrl: '/logo.png',
-        appWebsiteUrl: process.env.NEXT_PUBLIC_APP_URL,
       }}
     >
       {children}
