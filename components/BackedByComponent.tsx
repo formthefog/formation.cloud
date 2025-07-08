c9c4abba4c82e84ea1bc1c2c33f8f7fad045d524
hathbanger <hathbanger.ah@gmail.com>
2025-07-08 15:19:50 +0200

    logos

--- components/BackedByComponent.tsx
+++ components/BackedByComponent.tsx
@@ -3,50 +3,50 @@ const BackedBy = () => {
     <div className="relative w-full bg-white shadow-md md:shadow-none">
       <div className="container mx-auto max-w-[1280px]">
         <div className="grid grid-cols-2 md:grid-cols-7 h-auto md:h-[96px] items-center border border-gray-200 divide-y md:divide-y-0 divide-x divide-gray-200">
-          <div className="flex justify-center items-center h-[72px] md:h-full">
-            <img
-              src="/logos/alpha-dao.png"
-              alt="Alpha DAO"
-              className="w-auto max-h-12"
-            />
-          </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
-            <img
-              src="/logos/big-brain2.png"
-              alt="Big Brain"
-              className="w-auto max-h-12"
-            />
-          </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
+          <div className="flex justify-center items-center h-[96px] md:h-full">
             <img
               src="/logos/jump2.png"
               alt="Jump"
-              className="w-auto max-h-12"
+              className="w-auto max-h-36"
             />
           </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
-            <img
-              src="/logos/ngc-ventures.png"
-              alt="NGC Ventures"
-              className="w-auto max-h-12"
-            />
-          </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
+          <div className="flex justify-center items-center h-[96px] md:h-full">
             <img
               src="/logos/republic2.png"
               alt="Republic"
-              className="w-auto max-h-12"
+              className="w-auto max-h-24"
             />
           </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
+          <div className="flex justify-center items-center h-[96px] md:h-full">
+            <img
+              src="/logos/big-brain3.png"
+              alt="Big Brain"
+              className="w-auto max-h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center h-[96px] md:h-full">
+            <img
+              src="/logos/alpha-dao.png"
+              alt="Alpha DAO"
+              className="w-auto max-h-24"
+            />
+          </div>
+          <div className="flex justify-center items-center h-[96px] md:h-full">
+            <img
+              src="/logos/ngc-ventures.png"
+              alt="NGC Ventures"
+              className="w-auto max-h-16"
+            />
+          </div>
+          <div className="flex justify-center items-center h-[96px] md:h-full">
             <img
               src="/logos/taureon.png"
               alt="Taureon"
-              className="w-auto max-h-12"
+              className="w-auto max-h-24"
             />
           </div>
-          <div className="flex justify-center items-center h-[72px] md:h-full">
-            <img src="/logos/udao.png" alt="UDAO" className="w-auto max-h-12" />
+          <div className="flex justify-center items-center h-[96px] md:h-full">
+            <img src="/logos/udao.png" alt="UDAO" className="w-auto max-h-24" />
           </div>
         </div>
       </div>
